da4e3fab130ba961ecbb9eedf69d7dfa0399b19a
Daniel Samper <danielsamper@Mac-mini-de-Daniel.local>
2026-05-24 20:02:52 +0200

    Estructura MVC

---
+++ www/views/layout/footer.php
@@ -0,0 +1,7 @@
+ </div>
+     <!-- Pie de página --> 
+     <footer id="footer">
+        <p>Desarrollado por Daniel Samper &copy; <?= date('Y') ?></p>
+     </footer>
+</body>
+</html>
---
+++ www/views/layout/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Tienda de camisetas</title>
+</head>
+<body>
+    <div id="container">
+    <!-- Encabezado -->
+     <header id="header">
+        <div id="logo">
+            <img src="assets/img/camiseta.png" alt="Camiseta Logo">
+            <a href="index.php">
+                Tienda de camisetas
+            </a>
+        </div>  
+
+    </header>
+    <!-- Menu -->
+     <nav id="menu">
+        <ul>
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+            <li>
+                <a href="#">Tienda</a>
+            </li>
+            <li>
+                <a href="#">Contacto</a>
+            </li>
+            <li>
+                <a href="#">Mi cuenta</a>
+            </li>
+        </ul>
+    </nav>
+    <div id="content">
